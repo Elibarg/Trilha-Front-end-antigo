d4f0cb99893bf121cecd4a633434812c571d067e
Elibarg <calagabriel31@gmail.com>
2025-03-26 19:27:31 -0300

    js

--- JAVA SCRIPT/Coldigo Geladeiras/js/site.js
+++ JAVA SCRIPT/Coldigo Geladeiras/js/site.js
@@ -1,4 +1,38 @@
 function vaildaFaleConosco()
 {
-alert("achou!");
+    if(document.frmfaleconosco.txtnome.value=="")
+    {
+        alert("Preencha o campo Nome");
+        document.frmfaleconosco.txtnome.focus();
+        return false;
+    }
+
+    if(document.frmfaleconosco.txtfone.value=="")
+        {
+            alert("Preencha o campo Telefone");
+            document.frmfaleconosco.txtfone.focus();
+            return false;
+        }
+
+        if(document.frmfaleconosco.txtemail.value=="")
+            {
+                alert("Preencha o campo E-mail");
+                document.frmfaleconosco.txtemail.focus();
+                return false;
+            }
+
+            if(document.frmfaleconosco.selmotivo.value=="")
+                {
+                    alert("Preencha o campo Motivo");
+                    document.frmfaleconosco.selmotivo.focus();
+                    return false;
+                }
+
+                if(document.frmfaleconosco.txtcomentario.value=="")
+                    {
+                        alert("Preencha o campo Comentário");
+                        document.frmfaleconosco.txtcomentario.focus();
+                        return false;
+                    }
+    return true
 }
