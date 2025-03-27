2c8ae5f77752a195ae07b966eaa7ea4fab17860f
Elibarg <calagabriel31@gmail.com>
2025-03-26 21:19:53 -0300

    a

--- js folder/JAVA SCRIPT/Coldigo Geladeiras/js/site.js
+++ js folder/JAVA SCRIPT/Coldigo Geladeiras/js/site.js
@@ -36,3 +36,70 @@ function vaildaFaleConosco()
                     }
     return true
 }
+function verificarMotivo(motivo)
+{
+    //capturamo a estrutura da div cujo ID é opcaoProduto na variável elemento
+    var elemento = document.getElementById("opcaoProduto");
+    if(motivo == "PR")
+    {
+        //criamos um elemento (tag) <select> e gardamos na variável homônica
+        var select = document.createElement("select");
+        //setamos nesse novo select o atributo 'name' com o valor 'selproduto'
+        select.setAttribute("name","selproduto");
+        //Conteúdo altual da varialve selct:
+        // <select name="selproduto"></select>
+        
+        //Colocamos o option como "filho" da tag select criada
+        select.appendChild(option);
+        //Conteúdo atual da variálve select
+        //<select name="selproduto"><option value="">Escolhas</option></select>
+ 
+        //Criamos um Elemento (tag) <option> e gardamos na variável homônica
+        var option = document.createElement("option");
+        //setamos nesse novo option o atributo 'value' com o valor "FR"
+        option.setAttribute("value","FR");
+        //criamos um nó de texto "Freazer" e gravamos na variavel 'texto'
+        var texto = document.createTextNode("Freazer");
+        //Colocamos o nó de texto críado como "filho" da tag option criada
+        option.appendChild(texto);
+        //Conteúdo atual da variálve option
+        //<option value="FR">Freazer</option>
+
+        select.appendChild(option);
+        //Conteúdo atual da variálve select
+
+        //<select name="selproduto">
+        //<option value="">Escolhas</option><option value="FR">Freazer</option>
+        //</select>
+
+        //Criamos um Elemento (tag) <option> e gardamos na variável homônica
+        var option = document.createElement("option");
+        //setamos nesse novo option o atributo 'value' com o valor "GE"
+        option.setAttribute("value","GE");
+        //criamos um nó de texto "Geladeira" e gravamos na variavel 'texto'
+        var texto = document.createTextNode("Geladeira");
+        //Colocamos o nó de texto críado como "filho" da tag option criada
+        option.appendChild(texto);
+        //Conteúdo atual da variálve option
+        //<option value="GE">Geladeira</option>
+
+        //Colocamos o nó de texto críado como "filho" da tag option criada
+        select.appendChild(option);
+        //Conteúdo atual da variálve select
+
+        //<select name="selproduto">
+        //<option value="">Escolhas</option><option value="FR">Freazer</option><option value="GE">Geladeira</option>
+        //</select>
+
+        //Colocamos o nó de texto críado como "filho" da tag option criada
+        elemento.appendChild(select);
+        //se o valor da variável motivo no for "PE"
+    }else{
+        //se a div possuir algum "primeiro filho"
+        if(elemento.firstChild)
+        {
+           //remove ele
+           elemento.removeChild(elemento.firstChild);
+        }
+    }
+}
